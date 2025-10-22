da2ee06f6243272222418e284f91edc6f12edf5f
Kevin Matte <matte.kevin@gmail.com>
2025-10-22 13:41:28 -0600

    fileWatcher.py: Only monitoring changes, not opened or closed!

--- bin/fileWatcher.py
+++ bin/fileWatcher.py
@@ -12,7 +12,9 @@ import time
 from argparse import ArgumentParser, Namespace, RawTextHelpFormatter
 
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from watchdog.events import FileSystemEventHandler, DirCreatedEvent, FileCreatedEvent, DirDeletedEvent, \
+    FileDeletedEvent, DirModifiedEvent, FileModifiedEvent, DirMovedEvent, FileMovedEvent
+
 
 def process_args() -> tuple[Namespace, ArgumentParser]:
 
@@ -36,11 +38,13 @@ def process_args() -> tuple[Namespace, ArgumentParser]:
         """
 
     help_opt_verbose = "Print extra processing information to stdout."
+    help_opt_repeat = "After targets found, repeat monitor in 1 second"
 
     # Parse command-line argument
     arg_parser = ArgumentParser(formatter_class=RawTextHelpFormatter, description=help_program)
     arg_parser.add_argument('paths', nargs="*", help=help_args_paths)
     arg_parser.add_argument("-v", "--verbose", help=help_opt_verbose, default=False, action='store_true')
+    arg_parser.add_argument("-r", "--repeat", help=help_opt_repeat, default=False, action='store_true')
     args = arg_parser.parse_args()
 
     if len(args.paths) == 0:
@@ -61,23 +65,39 @@ class MonitorAnyFileChange(FileSystemEventHandler):
 
         self._target_name = target_name
         self._file_extensions = file_extensions
-        self._changed_found = False
+        self._files = set()
 
     def get_target_name(self) -> str:
         """Returns the target_name passed in the contsructor."""
-        return self._target_name if self._changed_found else False
+        return self._target_name
 
     def has_change(self) -> bool:
         """Returns True iff any file change event has occurred. """
-        return self._changed_found
+        return len(self._files) > 0
 
-    def on_any_event(self, event):
+    def get_files(self):
+        return self._files
+
+    def _handle_event(self, event):
         """Event handler for an Observer."""
         if self._file_extensions:
             for file_extension in self._file_extensions:
                 if event.src_path.endswith(file_extension):
-                    self._changed_found = True
+                    self._files.add(event.src_path)
                     break
+
+    def on_created(self, event: DirCreatedEvent | FileCreatedEvent) -> None:
+        self._handle_event(event)
+
+    def on_deleted(self, event: DirDeletedEvent | FileDeletedEvent) -> None:
+        self._handle_event(event)
+
+    def on_modified(self, event: DirModifiedEvent | FileModifiedEvent) -> None:
+        self._handle_event(event)
+
+    def on_moved(self, event: DirMovedEvent | FileMovedEvent) -> None:
+        self._handle_event(event)
+
 
 class FilesWatcher:
     """Main class for this script. See help at top of file."""
@@ -150,6 +170,12 @@ class FilesWatcher:
                 observer.stop()
                 observer.join()
 
+        if self.args.verbose:
+            for event_handler in self.event_handlers:
+                files = event_handler.get_files()
+                if files:
+                    print(f"Target {event_handler.get_target_name()}: {" ".join(files)}")
+
         # Return set of target names with changes.
         targets_changed = set(event_handler.get_target_name()
                               for event_handler in self.event_handlers
@@ -163,6 +189,13 @@ def main():
     files_watcher = FilesWatcher(args, parser)
     targets_changed = files_watcher.start()
     print(" ".join(targets_changed))
+
+    while args.repeat:
+        time.sleep(1)
+        print("---")
+        targets_changed = files_watcher.start()
+        print(" ".join(targets_changed))
+
     exit(0)
 
 if __name__ == "__main__":
--- bin/monitorBuild.sh
+++ bin/monitorBuild.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 
 cd "$(dirname $0)/.."
+source bin/funcs.ish
 
+which gtts-cli mpg123 >/dev/null && can_speak=1 || can_speak=0
+
+say "Starting Monitor"
 while [ 1 = 1 ]; do
   targets="$(bin/python.sh bin/fileWatcher.py \
     build_static@src/static:.js,.jsx,.css,.html \
@@ -9,15 +13,15 @@ while [ 1 = 1 ]; do
   "
   if :; then
       if [ -f build.locked ]; then
-        echo "File build.locked exists. Waiting."
+        say "File build.locked exists. Waiting."
         while [ -f build.locked ]; do
-          echo "Waiting for build.locked to go away"
+          say "Waiting for build.locked to go away"
           sleep 2
         done
       fi
+      say "Making ${targets}"
       make ${targets} build_done
-      echo "----------------------"
-      date
+      say "Done"
       echo "======================"
   fi
 done
