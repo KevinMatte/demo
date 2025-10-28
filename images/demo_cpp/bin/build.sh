14f04b892d4610c207426ac8e0ab32ac09c698e0
Kevin Matte <matte.kevin@gmail.com>
2025-10-28 15:05:16 -0600

    Setting up demo_cpp container.

---
+++ images/demo_cpp/bin/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$(basename $(pwd))" != "demo_cpp" ]; then
+  echo "In wrong directory: $(pwd)"
+  exit 1
+fi
+
+PROJ_ROOT="$(git rev-parse --show-toplevel)"
+. "${PROJ_ROOT}/.env"
+
+rm -fr build
+mkdir -p build/root
+mkdir -p backup
+
+cat > build/root/.my.cnf <<EOF
+[client]
+user=root
+password=${MARIADB_ROOT_PASSWORD}
+EOF
