d1d0feedfa31bdf8a3c3f5b3340dca13483fa7e5
Kevin Matte <matte.kevin@gmail.com>
2025-10-31 09:57:02 -0600

    installPackages.sh: Updated with minimal based on new OS installs.

--- bin/installPackages.sh
+++ bin/installPackages.sh
@@ -5,28 +5,31 @@
 #   see https://docs.docker.com/engine/install/linux-postinstall/
 # For nodejs see: https://nodesource.com/products/distributions
 
+# For Framework
+#  For 'play' used by say.py
+sudo apt update
+sudo apt install -y make sox libsox-fmt-mp3
 
-sudo apt install python3
-sudo apt install python3-venv
-sudo apt install python-is-python3
-sudo apt install make
-# For 'play' used by say.py
-sudo apt install sox
-sudo apt-get install libsox-fmt-mp3
+# For demo_ui
+sudo apt update
+sudo apt install -y python3 python3-venv python-is-python3
 
 # Fpr demo_cpp
 # Installed Qt tools from: https://www.qt.io/download-qt-installer-oss
-sudo apt install g++
 sudo apt update
-sudo apt install libxcb-cursor0 libxcb-cursor-dev
-sudo apt install -y qtcreator qtbase5-dev qt5-qmake cmake build-essential gdb g++
+sudo apt -y install g++ cmake build-essential gdb
+sudo apt install -y libasio-dev
 
-sudo apt purge -y qtcreator qtbase5-dev qt5-qmake
+# sudo apt install -y libxcb-cursor0 libxcb-cursor-dev
+# sudo apt install -y qtcreator qtbase5-dev qt5-qmake
 
-# cpprestsdk
-sudo apt-get install g++ git libboost-atomic-dev libboost-thread-dev libboost-system-dev libboost-date-time-dev libboost-regex-dev libboost-filesystem-dev libboost-random-dev libboost-chrono-dev libboost-serialization-dev libwebsocketpp-dev openssl libssl-dev ninja-build
-apt install libssl-dev libcrypto++-dev
-apt install libboost-all-dev libasio-dev
+#sudo apt install -y libboost-atomic-dev libboost-thread-dev libboost-system-dev \
+#  libboost-date-time-dev libboost-regex-dev libboost-filesystem-dev libboost-random-dev \
+#  libboost-chrono-dev libboost-serialization-dev
+#
+#sudo apt install -y libwebsocketpp-dev openssl libssl-dev ninja-build
+#sudo apt install libssl-dev libcrypto++-dev
+#sydo apt install libboost-all-dev
 
 
 
