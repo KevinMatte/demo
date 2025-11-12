11cadcff908e5333d76a757718b6e37a476a31e4
Kevin Matte <matte.kevin@gmail.com>
2025-11-12 15:26:47 -0700

    Playing with improved lock releases and thread sharing.

--- images/demo_cpp/src/SharedLock.cpp
+++ images/demo_cpp/src/SharedLock.cpp
@@ -8,42 +8,65 @@ using namespace std;
 
 // import mutex from C++ standard library
 #include <mutex>
+#include <iomanip>
 
+/**
+ * Increases the number of thread switching by passing off to a different waiting thread, if any.
+ */
 class PoliteLock {
+    // Key implementation part that supports notify_all() or notify_one().
     std::condition_variable m_cond;
     std::mutex m_cond_mutex;
+
+    // Lock state
     bool m_is_locked = false;
     thread::id m_id;
+
+    // Count of threads waiting for lock + 1 if is locked.
     int m_count = 0;
-//    std::mutex m_count_mutex;
 
 public:
     void lock() {
         std::unique_lock<std::mutex> lock(m_cond_mutex);
+
+        // Ignore lock() if thread already has it.
         if (m_is_locked && m_id == this_thread::get_id())
             return;
-//        m_count_mutex.lock();
         m_count++;
-//        m_count_mutex.unlock();
 
+        // If someone else is waiting, let one of them get the lock.
         m_cond.wait(lock, [this]() {
             return !m_is_locked && (m_count == 1 || m_id != this_thread::get_id());
         });
+
+        // Lock established.
         m_id = this_thread::get_id();
         m_is_locked = true;
     }
 
     void unlock() {
         std::unique_lock<std::mutex> lock(m_cond_mutex);
+
+        // Ignore unlock() if thread doesn't own lock.
         if (!m_is_locked || m_id != this_thread::get_id())
             return;
 
+        // Unlock complete
         m_count--;
         m_is_locked = false;
-        m_cond.notify_all();
+
+        // Notify any other thread.
+        m_cond.notify_one();
     }
 };
 
+/**
+ * Free up processing after a countdown to other threads when repeatedly needing locks.
+ *
+ * The PoliteLock enhances this by assuring a different thread will pick up the lock if it's waiting.
+ *
+ * @tparam T A mutex, PoliteLock or anything with a lock()/unlock() method.
+ */
 template <typename T>
 class LockCountdown {
     int m_maxCount = 0;
@@ -58,7 +81,11 @@ public:
         assert(m_maxCount > 0);
     };
 
-    void lock() {
+    /**
+     * Start a lock, and if it already has one, unlock and lock it again
+     * when the countdown completes to free up processing for other threads.
+     */
+    void relock() {
 
         if (!m_is_locked) {
             m_count = 1;
@@ -69,7 +96,7 @@ public:
             m_count++;
             if (m_count >= m_maxCount) {
                 unlock();
-                lock();
+                relock();
             }
         }
     }
@@ -86,6 +113,9 @@ public:
     }
 };
 
+// ========================================================================
+// Testing
+// ------------------------------------------------------------------------
 // Shared resource for testing.
 int number = 0;
 int switched_count = 0;
@@ -100,7 +130,7 @@ void increment(T& sharedLock, bool useCountdown){
     for(int i=0; i<1000; i++){
         // Lock the thread using lock
         if (useCountdown)
-            countDown.lock();
+            countDown.relock();
         else
             sharedLock.lock();
         if (this_thread::get_id() != id) {
@@ -118,7 +148,7 @@ void increment(T& sharedLock, bool useCountdown){
 
 template <typename T>
 void test(const char *name, bool useCountdown, int n=2) {
-    cout << name << (useCountdown ? " with countdown" : " without countdown") << ": " << n << " threads." << endl;
+    cout << setw(20) << right << name << (useCountdown ? " with countdown" : " without countdown") << ": " << n << " threads.";
 
     switched_count = 0;
     number = 0;
@@ -137,13 +167,15 @@ void test(const char *name, bool useCountdown, int n=2) {
     assert(number == n * 1000);
 
     // Print the number after the execution of both threads
-    cout << "Switched threads: " << switched_count << "\nNumber: " << number << "\n" << endl;
+    cout << " Switched threads: " << switched_count << endl;
 }
 
 int main()
 {
-    test<PoliteLock>("PoliteLock Test", true, 20);
-//    test<mutex>("mutex Test", 20);
+    test<PoliteLock>("PoliteLock Test", true, 200);
+    test<mutex>("mutex Test", true, 200);
+    test<PoliteLock>("PoliteLock Test", false, 200);
+    test<mutex>("mutex Test", false, 200);
 
     return 0;
 }
