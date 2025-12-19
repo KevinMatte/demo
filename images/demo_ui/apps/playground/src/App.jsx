292fc831db7a341e9215a3f9b3db16178094eb28
Kevin Matte <matte.kevin@gmail.com>
2025-12-19 13:44:16 -0700

    Extracted MenuSummary.jsx from Overview.jsx

--- images/demo_ui/apps/playground/src/App.jsx
+++ images/demo_ui/apps/playground/src/App.jsx
@@ -3,9 +3,10 @@ import "@/css/App.css";
 import appCSS from "@/css/App.module.css";
 import Menu from '@/parts/Menu.jsx';
 import DemoAnchor from "@/parts/Anchors.jsx";
+import {Anchor} from "@/parts/Anchors.jsx";
 import TabPages, {toolDefn as notesToolDefn} from '@docs/TabPages.jsx';
 import {menus_apps, menus_skeletons} from "@/vars/menus.jsx";
-import Overview, {toolDefn as overviewToolDefn} from '@/pages/docs/Overview.jsx';
+import MenuSummary, {toolDefn as overviewToolDefn} from '@docs/MenuSummary.jsx';
 import Build, {toolDefn as buildToolDefn} from "@/pages/docs/Build.jsx";
 import Containers, {toolDefn as dockerComposeToolDefn} from "@/pages/docs/Containers.jsx";
 
@@ -19,15 +20,26 @@ function App() {
     let topMenu = {...menus_skeletons, ...menus_apps};
 
     const notesMenu = {
-        "overview": {"label": "Overview", 'toolDefn': overviewToolDefn,
-            'page': (<Overview menu={topMenu} heading="Overview" handleClick={menuHandler}/>)},
+        "overview": {
+            "label": "Menu Summary", 'toolDefn': overviewToolDefn,
+            'page': (
+                <>
+                    YouTube:
+                    <Anchor path="https://youtu.be/fLLyFpopUcY">Playground Quick Tour</Anchor>
+                    <h2>Menu Summary</h2>
+                    <MenuSummary menu={topMenu} handleClick={menuHandler}/>
+                </>
+            )
+        },
         "build": {"label": "Build", 'toolDefn': buildToolDefn, 'page': (<Build/>)},
         "containers": {"label": "Docker Containers", 'toolDefn': dockerComposeToolDefn, 'page': (<Containers/>)},
     };
 
     const menu = {
-        "notes": {"label": "Notes", 'toolDefn': notesToolDefn,
-            'page': <TabPages tabMenu={notesMenu}/>},
+        "notes": {
+            "label": "Notes", 'toolDefn': notesToolDefn,
+            'page': <TabPages tabMenu={notesMenu}/>
+        },
         ...topMenu,
     };
 
--- images/demo_ui/apps/playground/src/pages/docs/MenuSummary.jsx
+++ images/demo_ui/apps/playground/src/pages/docs/MenuSummary.jsx
@@ -1,27 +1,21 @@
 import menuCss from "@/parts/css/Menu.module.css";
 import MenuButton from "@/parts/MenuButton.jsx";
-import {Anchor} from "@/parts/Anchors.jsx";
 import React from "react";
 
 export const toolDefn = {
-    "title": "Documentation: Demo Overview",
+    "title": "Documentation: Demo Menu",
     "description": <span><b>Documentation</b>: Demo Overview</span>,
     "toolDocs": null,
 };
 
-function Overview({heading, menu, handleClick}) {
+function MenuSummary({menu, handleClick}) {
     return (
-        <div>
-            YouTube:
-            <Anchor path="https://youtu.be/fLLyFpopUcY">Playground Quick Tour</Anchor>
-
-            <h2>{heading}</h2>
-            <ul>
-                <div style={{display: "table"}}>
+        <ul>
+            <div style={{display: "table"}}>
                 {Object.keys(menu).map(name => {
                         let item = menu[name];
                         return (
-                            <div style={{display: "table-row"}} key={name}  className={menuCss.menu}>
+                            <div style={{display: "table-row"}} key={name} className={menuCss.menu}>
                                 <div style={{display: "table-cell"}}>
                                     <div style={{float: "right", margin: "2px"}}>
                                         <MenuButton
@@ -36,15 +30,17 @@ function Overview({heading, menu, handleClick}) {
 
                                     </div>
                                 </div>
-                                <div style={{display: "table-cell", verticalAlign: "middle"}}>{item.toolDefn.description || item.toolDefn.title}</div>
+                                <div style={{
+                                    display: "table-cell",
+                                    verticalAlign: "middle"
+                                }}>{item.toolDefn.description || item.toolDefn.title}</div>
                             </div>
                         );
                     }
                 )}
-                </div>
-            </ul>
-        </div>
+            </div>
+        </ul>
     )
 }
 
-export default Overview;
+export default MenuSummary;
