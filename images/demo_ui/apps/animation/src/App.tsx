24fd786c7214cbf864cd2c7a5808074edcf6bf80
Kevin Matte <matte.kevin@gmail.com>
2026-02-03 20:39:34 -0700

    Starting Menu.

--- images/demo_ui/apps/animation/src/App.tsx
+++ images/demo_ui/apps/animation/src/App.tsx
@@ -2,6 +2,9 @@ import '@/App.css'
 import PaintApp from "./PaintApp.tsx";
 import {Button} from '@mui/material';
 import Shelf from "./utils/Shelf.tsx";
+import * as React from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 // import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
@@ -20,6 +23,15 @@ function App() {
         );
     }
 
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <>
             {/*<CssBaseline />*/}
@@ -27,6 +39,41 @@ function App() {
                 {/*<div className="flexVDisplay fill">*/}
                 <Shelf>
                     {backElement}: This is WIP. Nothing here to see. :-)
+                </Shelf>
+                <Shelf direction="row">
+                    <Button
+                        id="basic-button"
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                    >
+                        Button1
+                    </Button>
+                    <Button
+                        id="basic-button"
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                    >
+                        Button2
+                    </Button>
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        slotProps={{
+                            list: {
+                                'aria-labelledby': 'basic-button',
+                            },
+                        }}
+                    >
+                        <MenuItem onClick={handleClose}>Profile</MenuItem>
+                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                        <MenuItem onClick={handleClose}>Logout</MenuItem>
+                    </Menu>
                 </Shelf>
                 <Shelf flex="1">
                     <PaintApp/>
